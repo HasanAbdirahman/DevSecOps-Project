860e18df7d2e94b2f1e353e6ca42b242693b75db
HasanAbdirahman <108181341+HasanAbdirahman@users.noreply.github.com>
2025-08-26 12:29:04 -0400

    Update db.js
    Update db.js

--- api/models/db.js
+++ api/models/db.js
@@ -2,17 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql
-  .createPool({
+  .createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    database: process.env.DB_NAME
   });
 
 module.exports = db;
